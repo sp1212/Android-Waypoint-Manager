c025ed97bbc180cebe3ddbecf779e4ee8d99cf7f
Stuart Paine <scp4exq@virginia.edu>
2022-12-08 01:29:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android Waypoint Manager App
+## By scp4exq
+
+This application has been developed as a final project for UVA CS 4720 - Mobile Application Development
+during the Fall 2022 semester.
---
+++ app/src/main/java/edu/virginia/cs4720/finalproject/scp4exq/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.virginia.cs4720.finalproject.scp4exq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
