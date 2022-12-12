2cc9d071edb8fe4d6f01f8cfc990f8a41ad9cf35
Stuart Paine <scp4exq@virginia.edu>
2022-12-12 17:27:32 -0500

    Clear input upon device shake

--- app/src/main/java/edu/virginia/cs4720/finalproject/scp4exq/ActivityNew.kt
+++ app/src/main/java/edu/virginia/cs4720/finalproject/scp4exq/ActivityNew.kt
@@ -6,6 +6,10 @@ import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
@@ -30,6 +34,7 @@ import kotlinx.android.synthetic.main.activity_new.*
 import org.json.JSONObject
 import java.util.*
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 class ActivityNew : AppCompatActivity() {
 
@@ -47,11 +52,26 @@ class ActivityNew : AppCompatActivity() {
     private val PERMISSION_ID = 42
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
 
         supportActionBar?.title = "Waypoint Journal"
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -137,6 +157,44 @@ class ActivityNew : AppCompatActivity() {
         } else {
             input.toString()
         }
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > 12) {
+                Toast.makeText(applicationContext, "Shake detected.  Clearing fields...", Toast.LENGTH_SHORT).show()
+                itemTitle.text.clear()
+                chosenDate.text = ""
+                notesField.text.clear()
+                latField.text = ""
+                longField.text = ""
+                weatherIcon.setImageDrawable(null)
+                weatherIconUrl.text = ""
+                temp.text = ""
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
     }
 
     @SuppressLint("MissingPermission")
@@ -260,4 +318,9 @@ class ActivityNew : AppCompatActivity() {
  *  Author: Programming Guru
  *  Date: Dec 21, 2021
  *  URL: https://www.youtube.com/watch?v=wplXJJAS3Fg
+ *
+ *  Title: How to Detect Shake Event in Android?
+ *  Author: aashaypawar
+ *  Date: 14 Feb, 2022
+ *  URL: https://www.geeksforgeeks.org/how-to-detect-shake-event-in-android/
  ***************************************************************************************/
